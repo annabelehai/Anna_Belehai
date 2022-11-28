6d3680991ec1b3151ef4fa0a3f725965653f9d23
Anna Belehai <abeleh@softserveinc.com>
2022-11-28 16:10:08 +0200

    commit changes

---
+++ HOMEWORK1/index.js
@@ -0,0 +1,21 @@
+// task2
+//console.log("Belehai");
+//task3
+// let color1 = "black";
+// let color2 = "blue";
+// console.log(color1,color2);
+// color1 = color2;
+// console.log(color1,color2);
+//task4
+// let hotel = {
+//     name: "Grand Hotel",
+//     floor: 5, 
+//     pool: true,
+//     guests: undefined,
+//     age: null
+// }
+// console.log(hotel);
+//task5
+// let isAdult = confirm("Are you over 18 years old?");
+// alert (isAdult);
+//task6
