2d1647a116b25c65891ff6bbd4cf8fe88f0ac8bb
Anna Belehai <abeleh@softserveinc.com>
2022-11-29 14:02:46 +0200

    commit changes

--- HOMEWORK1/index.js
+++ HOMEWORK1/index.js
@@ -19,3 +19,21 @@
 // let isAdult = confirm("Are you over 18 years old?");
 // alert (isAdult);
 //task6
+// let firstName = "Anna";
+// console.log(typeof firstName);
+// let lastName = "Belehai";
+// console.log(typeof lastName);
+// let group = "Lv-732.TAQC.Retraining";
+// console.log(typeof group);
+// const birthday = 1998;
+// console.log(typeof birthday);
+// let unmarriedStatus = true;
+// console.log(typeof unmarriedStatus);
+
+// console.log(birthday,unmarriedStatus,firstName,lastName,group);
+
+// let data1 = null
+// console.log(typeof data1);
+// let data2 = undefined
+// console.log(typeof data2);
+// task7
