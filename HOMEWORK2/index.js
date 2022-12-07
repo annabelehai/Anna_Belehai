fb3d99a49895901f8966f9da332b75f462fae9eb
Anna Belehai <abeleh@softserveinc.com>
2022-12-08 00:31:06 +0200

    commit

---
+++ HOMEWORK2/index.js
@@ -0,0 +1,35 @@
+// //task1
+// let x = 1;
+// let y = 2;
+// y = "2";
+// let res1 = x + y;
+// console.log(res1); // "12"
+// console.log(typeof res1); // "string"
+
+// x = true;
+// let res2 = x + y;
+// console.log(res2); // "true2"
+// console.log(typeof res2); // "string"
+
+// let res3 = (y != x)
+// console.log(res3); // true
+// console.log(typeof res3); // "boolean"
+
+// x = 5*'data1';
+// y = 5*'data2';
+// let res4 = x + y;
+// console.log(res4); // NaN
+// console.log(typeof res4); // "number"
+
+// //task2
+// let data1 = prompt("Enter your number");
+// console.log (data1 % 7 == 0 );//кратним числу 7.
+// console.log (data1 % 2 >= 0 );//парним додатним
+
+// //task3
+// const arr1 = [1,"element2", true, null ];
+// console.log(arr1.length);//число елементів, яке зберігається в масиві;
+// arr1[4] = prompt ("Enter your data");
+// console.log( arr1[4] );
+// arr1.shift();
+// alert(arr1);
