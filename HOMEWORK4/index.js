ccc15934cf95f7878c73dab5a0e85045ba9c398c
Anna Belehai <abeleh@softserveinc.com>
2023-01-02 22:09:29 +0200

    commit

---
+++ HOMEWORK4/index.js
@@ -0,0 +1,41 @@
+// task1функцію calcRectangleArea(width, height), яка приймає 2 параметри ширина прямокутника width і висота прямокутника 
+// height і обраховує його площу. Передбачити припинення виконання програми і генерацію винятку у випадку,
+//  якщо функції передано не числові параметри.
+
+// let height = prompt ("Enter your value for height");
+// let width = prompt("Enter your value for width");
+// function calcRectangleArea( ) {
+// if (height|width == String);{
+//     throw new Error("Line length greater than 4 characters");
+// }else{
+//     return
+// }
+// console.log(calcRectangleArea);
+
+//  S = 1/2 * width * height;
+//  console.log(S);
+
+
+//  task2   
+// Напишіть функцію checkAge(), яка пропонуватиме юзеру ввести свій вік і генерувати в модальному вікні помилки у випадку, коли: 
+// 	- юзер ввів порожню стрічку (наприклад “The field is empty! Please enter your age”), 
+// 	- нечислове значення
+// 	- вік юзера менше 14 років. 
+
+// function checkAge(){
+//     let age = +prompt("Please enter your age");
+//     if (age == ''){
+//         throw new Error("The field is empty! Please enter your age");
+//     }
+// else if (isNaN(age))
+// throw new Error("your value is not a number");
+
+// else if (age < 14)
+// throw new Error("you too young");
+// }
+
+// try{
+//     checkAge();
+// } catch(exception){
+//    console.log(exception.message);
+// }
