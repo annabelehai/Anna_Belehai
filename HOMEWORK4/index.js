6aa04c660096bf773ecc877b31ef111acf5b777f
Anna Belehai <abeleh@softserveinc.com>
2023-01-04 20:39:55 +0200

    commit

--- HOMEWORK4/index.js
+++ HOMEWORK4/index.js
@@ -22,9 +22,6 @@
 // }
 
 
-
-
-
 //  task2   
 // Напишіть функцію checkAge(), яка пропонуватиме юзеру ввести свій вік і генерувати в модальному вікні помилки у випадку, коли: 
 // 	- юзер ввів порожню стрічку (наприклад “The field is empty! Please enter your age”), 
@@ -51,3 +48,82 @@
 //task 3 функцію showMonthName(month), в якій параметр month – це порядковий номер місяця в році. Функція повертає назву місяця
 //  відповідно до введеного номера місяця. У випадку некоректного вводу кидається ексепшн у вигляді об’єкта класу MonthException з 
 //  /повідомленням 'Incorrect month number'.
+// class MonthException{
+//     constructor(message){
+//         this.message = message;
+//         this.name = "MonthException";
+//     }
+// }
+
+// function showMonthName(month){
+// month = +prompt("Please enter your number");
+// if (month== 1){
+// alert ("January");
+// }
+// else if (month== 2){
+// alert ("February");
+// }
+// else if (month== 3){
+// alert ("March");
+// }
+// else if (month== 4){
+// alert ("April");
+// }
+// else if (month== 5){
+// alert ("May");
+// }
+// else if (month== 6){
+// alert ("June");
+// }
+// else if (month== 7){
+// alert ("July");
+// }
+// else if (month== 8){
+// alert ("August");
+// }
+// else if (month== 9){
+// alert ("September");
+// }
+// else if (month== 10){
+// alert ("October");
+// }
+// else if (month== 11){
+// alert ("November");
+// }
+// else if (month== 12){
+// }
+// else if (month > 12){
+// throw new MonthException("Incorrect month number");       
+// }
+// }
+
+// try{
+//     let result = showMonthName();
+//     console.log(result);
+// } catch(exception){
+//    console.log(exception.message);
+// }
+// task 4
+// а) Реалізуйте функцію showUser(id), яка приймає параметром користувацьке id і повертає об’єкт, який містить значення переданої id. Також функція викидає помилку 
+// у разі якщо введено від’ємне id.
+
+//  b) Реалізуйте функцію showUsers(ids), яка приймає параметром масив користувацьких айді ids, перевіряє з використанням функції showUser() кожен елемент масиву ids
+//  на коректність, в разі виключної ситуації виводить повідомлення про помилку. Функція showUsers(ids) повертає масив об’єктів, де значеннями ключів є коректні елементи ids.
+
+// а)
+// function showUser(){
+//     this.id = +prompt("Please enter your value");
+//     if (this.id < 0){
+//         throw new Error("your value for id is negative");
+//     }
+// else if (this.id >0){
+//     return this.id;
+// }
+// }
+// try{
+//     let result = showUser();
+//    console.log(result);
+// } catch(exception){
+//    console.log(exception.message);
+// }
+// b)
