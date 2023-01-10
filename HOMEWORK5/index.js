115cc57f0d1522c1258711a64d72069dd6948498
Anna Belehai <abeleh@softserveinc.com>
2023-01-10 14:24:13 +0200

    commit

---
+++ HOMEWORK5/index.js
@@ -0,0 +1,29 @@
+// Task1 функцію propsCount(currentObject), яка приймає об’єкт і визначає кількість властивостей цього об’єкта.
+
+// let mentor = {
+//     course: "JS fundamental",
+//     duration: 3,
+//     direction: "web-development"
+// }
+
+// function propsCount(object){
+//     return Object.keys(object).length
+// }
+
+// console.log(propsCount(mentor));
+
+// Task2 функцію showProps(obj), яка приймає даний об’єкт і виводить список його властивостей записаних в масив. Виведіть також масив значень властивостей об’єкта.
+
+// let Person = {
+//     name: "Anna",
+//     country: "Ukraine",
+//     age: 24,
+//     direction: "QC",
+//     married: false,
+// }
+// function showProps(obj){
+
+//     return (Object.keys(obj))
+// }
+// console.log(showProps(Person));
+// console.log(Object.values(Person));
