dc81669b2b9ff61b830c76c232477b33ca3a8550
Anna Belehai <anna.belehai@henryscheinone.com>
2023-02-16 21:55:43 +0200

    commit

---
+++ HOMEWORK7/index.js
@@ -0,0 +1,33 @@
+// task 1
+
+// let arr = ["Tom", "Sam", "Ray", "Bob"];
+// let [firstArr, secondArr, thirddArr] = arr;
+// console.log(firstArr); // "Tom"
+// console.log(secondArr); // "Sam"
+// console.log(thirddArr); // [Bob]
+
+// task 2
+
+// let data = {
+//     names: ["Sam", "Tom", "Ray", "Bob"],
+//     ages: [20, 24, 22, 26],
+//  };​
+
+ 
+//  let /* Ваш код */ = data;
+
+//  console.log(name2); // "Tom"
+//  console.log(age2); // 24
+//  console.log(name4); // "Bob"
+//  console.log(age4); // 26
+ 
+ //task3
+
+//  function mul() {
+// const numbers = Array.prototype.slice.call(arguments);
+//   return numbers.reduce((a,b) => a + b);
+// };
+
+//  console.log(mul(1, "str", 2, 3, true)); // 6
+//  console.log(mul(null, "str", false, true)); // 0
+ 
